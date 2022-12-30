d8b4df6491c28294ff536e0cdb014628b189cabf
Julia Topolska <101990409+jtopolska@users.noreply.github.com>
2022-12-30 21:21:42 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,15 +3,11 @@ import './App.css';
 import Footer from './Footer';
 import HeaderAndItems from './Header';
 import Slider from './Slider';
-// import Header from './Header';
-// import Items from './Items';
 
 function App() {
   return (
     <div className="App">
       <HeaderAndItems />
-      
-      {/* <Items /> */}
       <About />
       <Slider />
       <Footer />
